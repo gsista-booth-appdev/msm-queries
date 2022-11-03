4df19b27929d92ef32395058e054fb5d5a7c2b88
gsista-booth <gsista@chicagobooth.edu>
2022-11-03 07:03:35 +0000

    Eldest Director complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,9 @@ class DirectorsController < ApplicationController
   render({:template => "director_templates/index"})
   end
   
+  def wisest
+    @oldest = Director.where.not({:dob => nil }).order({:dob => :asc }).first
+
+    render({:template => "director_templates/eldest"})
+  end
 end
